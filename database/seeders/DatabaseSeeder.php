3e2c275327a7ca62209ce1eea2c6cef91a72499a
Jacob Gardiner <jacobegardiner@gmail.com>
2022-03-25 15:36:44 -0400

    Add auth scaffolding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory(5)->create();
+        User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
         $this->call(DifficultySeeder::class);
         $this->call(QuestionSeeder::class);
     }
